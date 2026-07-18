b0d03180908801093419da8ba3e91488f2b7c930
salud54bienestar-del <salud54bienestar@gmail.com>
2026-07-18 17:56:22 -0500

    Create sistemas.js
    Registro de sistemas de Village Soul encargado de identificar los módulos activos del Soul Engine.

---
+++ servidor/sistemas.js
@@ -0,0 +1,14 @@
+const sistemas = [
+  "nucleo_mundo",
+  "almas",
+  "memorias",
+  "relaciones",
+  "eventos",
+  "decisiones",
+  "comunicacion"
+];
+
+console.log("Sistemas registrados:");
+sistemas.forEach((sistema) => {
+  console.log("- " + sistema);
+});
