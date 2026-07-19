fd448e8cfced770465e1ee9a76f3af54b3327fa2
salud54bienestar-del <salud54bienestar@gmail.com>
2026-07-19 13:58:02 -0500

    Update sistemas.js
    Registro central de sistemas de Village Soul encargado de identificar los módulos que forman parte del mundo. Incluye sistemas de inteligencia artificial, relaciones, familia, crecimiento, eventos, habilidades y evolución de los habitantes.

--- servidor/sistemas.js
+++ servidor/sistemas.js
@@ -1,26 +1,66 @@
 // Registro de sistemas de Village Soul
 
 const sistemas = [
+
   "nucleo_mundo",
+
   "mundo",
+
   "almas",
+
   "memorias",
+
   "emociones",
+
   "relaciones",
-  "objetivos",
-  "decisiones",
-  "eventos",
-  "comunicacion",
+
   "familias",
+
+  "etapas_vida",
+
+  "crecimiento",
+
+  "embarazo",
+
+  "nacimientos",
+
+  "adopciones",
+
+  "matrimonio",
+
+  "divorcio",
+
+  "custodia",
+
+  "objetivos",
+
+  "decisiones",
+
+  "eventos",
+
+  "comunicacion",
+
   "cultura",
+
   "recursos",
+
+  "profesiones",
+
+  "habilidades",
+
   "historia"
+
 ];
+
 
 console.log("Sistemas registrados:");
 
+
 sistemas.forEach((sistema) => {
+
   console.log("- " + sistema);
+
 });
+
 
 module.exports = sistemas;
