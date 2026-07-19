b4bd4bdf2ad3fc8aad22283e1e953d1bd9107dec
salud54bienestar-del <salud54bienestar@gmail.com>
2026-07-19 13:54:03 -0500

    Update relaciones.js
    Sistema de relaciones de Village Soul encargado de crear y gestionar los vínculos entre habitantes. Controla amistad, confianza, romance, compromiso, matrimonio, divorcio, historia de relaciones y límites familiares para mantener interacciones coherentes dentro del mundo.

--- servidor/relaciones.js
+++ servidor/relaciones.js
@@ -1,32 +1,81 @@
 // Sistema de relaciones de Village Soul
 
 const cargarArchivo = require("./cargador_datos.js");
+const crearEvento = require("./eventos.js");
+const crearMemoria = require("./memorias.js");
+
 
 function crearRelacion(habitante1, habitante2, tipo) {
 
     const datos = cargarArchivo("../datos/relaciones.json");
+
 
     if (!datos) {
         console.log("No se pudieron cargar las relaciones.");
         return null;
     }
 
+
     const relacion = {
+
         id: datos.relaciones.length + 1,
+
         habitante_1: habitante1,
         habitante_2: habitante2,
+
         tipo: tipo,
+
         confianza: 0,
-        estado: "nuevo"
+
+        romance: 0,
+
+        estado: "nuevo",
+
+        estado_pareja: "ninguno",
+
+        compatibilidad: 50,
+
+
+        limites:
+
+        {
+            parentesco: false,
+            relacion_permitida: true
+        },
+
+
+        historia: [],
+
+        eventos: []
+
     };
 
+
     datos.relaciones.push(relacion);
+
+
+
+    crearEvento(
+        "Nueva relación",
+        "Se creó una relación de tipo: " + tipo
+    );
+
+
+    crearMemoria(
+        habitante1,
+        "relacion",
+        "Comenzó una relación con el habitante " + habitante2,
+        "media"
+    );
+
 
     console.log("Nueva relación creada:");
     console.log(relacion);
 
+
     return relacion;
 }
+
 
 
 // Prueba inicial
@@ -34,7 +83,9 @@ function crearRelacion(habitante1, habitante2, tipo) {
 crearRelacion(
     1,
     2,
-    "amistad"
+    "desconocidos"
 );
+
+
 
 module.exports = crearRelacion;
