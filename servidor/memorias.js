8741345ca2d86e4f145a3373d64aec50ed3124c4
salud54bienestar-del <salud54bienestar@gmail.com>
2026-07-19 14:01:34 -0500

    Update memorias.js
    Sistema de memorias de Village Soul encargado de almacenar experiencias importantes de los habitantes. Las memorias influyen en emociones, decisiones, relaciones y comportamiento, permitiendo que cada alma tenga una historia única basada en sus experiencias.

--- servidor/memorias.js
+++ servidor/memorias.js
@@ -2,40 +2,98 @@
 
 const cargarArchivo = require("./cargador_datos.js");
 
-function crearMemoria(habitante_id, tipo, descripcion, importancia) {
+
+function crearMemoria(
+    habitante_id,
+    tipo,
+    descripcion,
+    importancia,
+    personas = [],
+    emocion = "neutral"
+) {
+
 
     const datos = cargarArchivo("../datos/memorias.json");
 
+
     if (!datos) {
+
         console.log("No se pudieron cargar las memorias.");
+
         return null;
+
     }
 
+
+
     const memoria = {
+
+
         id: datos.memorias.length + 1,
+
+
         habitante_id: habitante_id,
+
+
         tipo: tipo,
+
+
         descripcion: descripcion,
+
+
         importancia: importancia,
+
+
+        emocion: emocion,
+
+
+        personas_relacionadas: personas,
+
+
+        afecta_comportamiento: importancia === "alta",
+
+
         fecha: new Date().toISOString()
+
+
     };
+
+
 
     datos.memorias.push(memoria);
 
+
+
     console.log("Nueva memoria creada:");
+
     console.log(memoria);
 
+
+
     return memoria;
+
 }
+
 
 
 // Prueba inicial
 
 crearMemoria(
+
     1,
-    "exploracion",
-    "Luna descubrió una nueva zona del mundo.",
-    "media"
+
+    "familia",
+
+    "El habitante creó un vínculo importante con otro miembro de la comunidad.",
+
+    "media",
+
+    [2],
+
+    "felicidad"
+
 );
+
+
 
 module.exports = crearMemoria;
