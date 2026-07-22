c299678abb87f0b8ce6ac4815d29e32cfc335f9e
salud54bienestar-del <salud54bienestar@gmail.com>
2026-07-21 20:07:41 -0500

    Update cargador_datos.js

--- servidor/Sistemas/cargador_datos.js
+++ servidor/Sistemas/cargador_datos.js
@@ -1,38 +1,132 @@
-// Cargador de datos de Village Soul
+// Guardador de datos de Village Soul
 
 const fs = require("fs");
 const path = require("path");
 
-function cargarArchivo(nombre) {
 
-    try {
+
+// =================================
+// GUARDAR ARCHIVO JSON
+// =================================
+
+function guardarArchivo(
+    nombre,
+    datos
+){
+
+    try{
 
         // Eliminar ./ o ../ del inicio
-        nombre = nombre.replace(/^(\.\.\/|\.\/)+/, "");
 
-        // Siempre buscar dentro de la carpeta datos
-        const rutaCompleta = path.join(
-            __dirname,
-            "..",
-            nombre
+        nombre = nombre.replace(
+
+            /^(\.\.\/|\.\/)+/,
+
+            ""
+
         );
 
-        const datos = fs.readFileSync(rutaCompleta, "utf8");
 
-        return JSON.parse(datos);
 
-    } catch (error) {
+        // Buscar siempre dentro de la carpeta datos
 
-        console.log("===============================");
-        console.log("ERROR CARGANDO ARCHIVO");
-        console.log("===============================");
-        console.log("Archivo:", nombre);
-        console.log("Ruta buscada:", path.join(__dirname, "..", nombre));
-        console.log("Motivo:", error.message);
+        const rutaCompleta = path.join(
 
-        return null;
+            __dirname,
+
+            "..",
+
+            nombre
+
+        );
+
+
+
+        fs.writeFileSync(
+
+            rutaCompleta,
+
+            JSON.stringify(
+
+                datos,
+
+                null,
+
+                2
+
+            ),
+
+            "utf8"
+
+        );
+
+
+
+        return true;
+
+    }
+
+    catch(error){
+
+
+        console.log(
+
+            "==============================="
+
+        );
+
+        console.log(
+
+            "ERROR GUARDANDO ARCHIVO"
+
+        );
+
+        console.log(
+
+            "==============================="
+
+        );
+
+        console.log(
+
+            "Archivo:",
+
+            nombre
+
+        );
+
+        console.log(
+
+            "Ruta:",
+
+            path.join(
+
+                __dirname,
+
+                "..",
+
+                nombre
+
+            )
+
+        );
+
+        console.log(
+
+            "Motivo:",
+
+            error.message
+
+        );
+
+
+
+        return false;
+
     }
 
 }
 
-module.exports = cargarArchivo;
+
+
+module.exports = guardarArchivo;
